4f4e39401dbe9985c5a5bc11a92bf39465e7086f
Carl <meatballsofmeatballs@gmail.com>
2023-07-28 13:58:30 +0200

    test

--- bin/test-exercises.sh
+++ bin/test-exercises.sh
@@ -1,12 +1,12 @@
 test_run() {
     dic=$(mktemp -d)
     echo "Testing $1"
+    cp -r $1 $dic
     source_file="$(jq -r '.files.solution[0]' $1/.meta/config.json)"
-    test_file="$(jq -r '.files.test[0]' $1/.meta/config.json)"
-    echo "source_file: ${source_file}"
-    cat "$1/.meta/src/Example.re" > "${dic}/${source_file}"
-    cat "$1/${test_file}" > "${dic}/${test_file}"
+    cat "$1/.meta/src/Example.re" > "${dic}/$(basename $1)/${source_file}"
+    pushd "$dic/$(basename $1)" > /dev/null
     npm i && npm run build && npm run test:ci || exit 1
+    popd > /dev/null
 }
 
 for exercise in ./exercises/practice/*; do
