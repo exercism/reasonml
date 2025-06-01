50220e327758ac64cd7bc262d6cc32c2678dacec
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-06-01 13:00:33 +0200

    Update and simplify new exercise script
    [skip ci]

--- bin/new-exercise.sh
+++ bin/new-exercise.sh
@@ -6,6 +6,3 @@
 
 cp -r ./template ./exercises
 mv ./exercises/template ./exercises/$1
-find ./exercises/$1/ -type f | xargs sed -i "s/"'$(name)'"/$1/g"
-test_file=`echo $1_test.re | sed -e "s/\b\(.\)/\u\1/"`
-touch ./exercises/$1/__tests__/$test_file
