f2f54d3e0981fa2c426025a388f4b058c904250f
Kevin Gathuku <kevgathuku@users.noreply.github.com>
2025-06-02 13:37:29 +0000

    Fixes to new exercise template

--- bin/new-exercise.sh
+++ bin/new-exercise.sh
@@ -6,3 +6,8 @@
 
 cp -r ./template ./exercises
 mv ./exercises/template ./exercises/$1
+find ./exercises/$1/ -type f | xargs sed -i "s/"'$(name)'"/$1/g"
+lib_file=`echo $1.re | sed -e "s/\b\(.\)/\u\1/"`
+test_file=`echo $1_test.re | sed -e "s/\b\(.\)/\u\1/"`
+mv ./exercises/$1/lib/Example.re ./exercises/$1/lib/$lib_file
+mv ./exercises/$1/test/Example_test.re ./exercises/$1/test/$test_file
