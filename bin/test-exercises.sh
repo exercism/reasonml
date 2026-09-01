dabf388421bd2c4a0fd821829c22596c09126723
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2026-09-01 13:43:48 -0700

    Update test CI (#465)
    * Update test CI
    
    * Add missing package lock file

--- bin/test-exercises.sh
+++ bin/test-exercises.sh
@@ -1,20 +1,30 @@
-test_run() {
-    dic=$(mktemp -d)
-    echo "Testing $1"
-    cp -r $1 $dic
-    source_file="$(jq -r '.files.solution[0]' $1/.meta/config.json)"
-    cat "$1/.meta/src/Example.re" > "${dic}/$(basename $1)/${source_file}"
-    pushd "$dic/$(basename $1)" > /dev/null
-    npm i && npm run build && npm run test:ci || exit 1
-    popd > /dev/null
-}
+#!/usr/bin/env bash
+
+test_run() (
+    exercise=$1
+    tmp_dir=$(mktemp -d) || exit 1
+    trap 'rm -rf -- "$tmp_dir"' EXIT
+
+    echo "Testing $exercise"
+    cp -r "$exercise" "$tmp_dir" || exit 1
+
+    source_file=$(jq -er '.files.solution[0]' "$exercise/.meta/config.json") || exit 1
+
+    exercise_dir="$tmp_dir/$(basename "$exercise")"
+    cp "$exercise/.meta/src/Example.re" "$exercise_dir/$source_file" || exit 1
+
+    cd "$exercise_dir" || exit 1
+    npm ci --loglevel=error --no-audit --no-fund > /dev/null &&
+        npm run --silent build &&
+        npm run --silent test:ci
+)
 
 for exercise in ./exercises/practice/*; do
-    test_run ${exercise} 
+    test_run "$exercise" || exit 1
 done
 
 if [ -d "./exercises/concept" ]; then
     for exercise in ./exercises/concept/*; do
-        test_run ${exercise}
+        test_run "$exercise" || exit 1
     done
 fi
